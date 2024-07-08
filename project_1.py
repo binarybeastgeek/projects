633fbbb77ebbd4141ff800a1052915d1022c2dea
OscarDev <binarybeastgeek@gmail.com>
2024-07-08 17:57:47 -0500

    Se agregó el juego 'Adivina el número'.

---
+++ project_1.py
@@ -0,0 +1,13 @@
+"""
+Adivina el número
+"""
+import random
+
+numero_usuario = int(input("Ingrese un número del 1 al 3: "))
+numero_maquina = random.randint(1,3)
+
+
+if numero_usuario == numero_maquina:
+    print("Muy bien, adivinaste el número.")
+else:
+    print("No adivinaste el número")
